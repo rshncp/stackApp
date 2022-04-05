497d77eeef33f9cc19c5390d21cf49e90ac24e05
ROUDHAYA <roshni.udhaya@capgemini.com>
2022-04-05 17:32:54 +0200

    first commit

---
+++ src/main/java/com/example/stackapp/StackAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.stackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stackapp/StackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
